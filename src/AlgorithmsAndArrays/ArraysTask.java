340ad75ea8a2a4e7dddf412602f5b311f29b055f
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-18 22:40:24 +0600

    added descriptions

--- src/AlgorithmsAndArrays/ArraysTask.java
+++ src/AlgorithmsAndArrays/ArraysTask.java
@@ -1,10 +1,14 @@
 package AlgorithmsAndArrays;
 
 public class ArraysTask {
+
+    // seasonsArray - return a String[] array that will list all the seasons of the year, starting with winter.
     public static String[] seasonsArray() {
         return new String[]{"Winter", "Spring", "Summer", "Autumn"};
     }
 
+    // generateNumbers - generate an int[] array of consecutive positive integers starting at 1 of the given length
+    // (length parameter > 0)
     public static int[] generateNumbers(int length) {
         int[] numbers = new int[length];
         for (int i = 0; i < numbers.length; i++) {
@@ -13,6 +17,7 @@ public class ArraysTask {
         return numbers;
     }
 
+    // totalSum - find the sum of all elements of the int[] array.
     public static int totalSum(int[] arr) {
         int result = 0;
         for (int el : arr) {
@@ -21,6 +26,8 @@ public class ArraysTask {
         return result;
     }
 
+    // findIndexOfNumber - return the index of the first occurrence of number in the arr array. If there is no such
+    // element in the array, return -1.
     public static int findIndexOfNumber(int[] arr, int elem) {
         for (int i = 0; i < arr.length; i++) {
             if (elem == arr[i]) {
@@ -29,6 +36,8 @@ public class ArraysTask {
         }
         return -1;
     }
+
+    // reverseArray - return the new String[] array obtained from the arr array by reversing the order of the elements.
     public static String[] reverseArray(String[] arr){
         for (int i = 0; i < arr.length / 2; i++) {
             String temp = arr[i];
@@ -37,6 +46,9 @@ public class ArraysTask {
         }
         return arr;
     }
+
+    // getOnlyPositiveNumbers - return new int[] array obtained from arr int[] array by choosing positive numbers only.
+    // P.S. 0 is not a positive number
     public static int[] getOnlyPositiveNumbers(int[] arr){
         int newSize = 0;
         for(int i = 0; i < arr.length; i++){
